ba6ae985ec8b7c17f4ad42e8ad37424fa4f9c1fa
Filipe <filipelfiuza@gmail.com>
2026-01-17 18:10:57 -0300

    starting micro-services activity

---
+++ Atividade técnica microserviços/product-service/src/test/java/filipe/com/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package filipe.com.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
